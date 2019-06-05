443e07d3f7971a26cfb64d467d66a813bab6cb0c
ThienLe029 <50948507+ThienLe029@users.noreply.github.com>
2019-06-05 10:35:25 +0700

    Add files via upload

---
+++ Sync_vs_Async/index2.js
@@ -0,0 +1,27 @@
+var axios = require('axios');
+
+axios.get('https://jsonplaceholder.typicode.com/todos/1')
+  .then(function (response) {
+    // handle success
+    console.log(response);
+  })
+  .catch(function (error) {
+    // handle error
+    console.log(error);
+  })
+  .finally(function () {
+    // always executed
+  });
+
+axios.get('https://jsonplaceholder.typicode.com/todos/2')
+  .then(function (response) {
+    // handle success
+    console.log(response);
+  })
+  .catch(function (error) {
+    // handle error
+    console.log(error);
+  })
+  .finally(function () {
+    // always executed
+  });
